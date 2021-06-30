a8871f850ec411218b6d7286d9c6aa9a7eb03416
mahmudulmurad <mahmudul.murad7@gmail.com>
2021-07-01 00:27:45 +0600

    updateAPI

--- src/routers/user.js
+++ src/routers/user.js
@@ -6,7 +6,6 @@ const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
-    console.log(user);
     try {
         await user.save()
         const token = await user.generateAuthToken()
