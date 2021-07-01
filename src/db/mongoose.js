a3748c1b0cd0f638e6f6f1e38902c7b16374c96d
mahmudulmurad <mahmudul.murad7@gmail.com>
2021-07-01 10:15:30 +0600

    db cluster fixing

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,3 +5,11 @@ mongoose.connect(process.env.MONGODB_URL, {
     useCreateIndex: true,
     useFindAndModify: false
 })
+const db = mongoose.connection;
+db.on("error", (err) => {
+    console.log(err.message);
+});
+
+db.once("open", () => {
+    console.log("MongoDB connection success");
+});
